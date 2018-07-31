4460c3c1d004a30a42062d676574d3f02bbcfac6
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-07-31 09:34:22 +0200

    Make crawled articles into objects by adding a class

--- Crawler.py
+++ Crawler.py
@@ -5,22 +5,30 @@ r = requests.get("http://python.beispiel.programmierenlernen.io/index.php")
 from bs4 import BeautifulSoup
 doc = BeautifulSoup(r.text, "html.parser")
 
+# This class covers now all our crawled data: title, emoji, content, image
+class CrawledArticle():
+    def __init__(self, tiitle, emoji, content, image):
+        self.title = title
+        self.emoji = emoji
+        self.content = content
+        self.image = image
+
+# Create a list with intention to append all CrawledArticle here
+articles = []
+
 # All infos we want to crawl are in a <div class="card">
 for card in doc.select(".card"):
     emoji = card.select_one(".emoji").text
     content = card.select_one(".card-text").text
-
-    # Give me second span elements in class="card.title"
     title = card.select(".card-title span")[1].text
-
-    # Give me the link in src attribute of img tag
     image = card.select_one("img").attrs["src"]
 
+    # Append CrwaledArticle to artice to articles-list
+    crawled = CrawledArticle(title, emoji, content, image)
+    articles.append(crawled)
 
-    print(image)
-    print(title)
-    print(emoji)
-    print(content)
+    print(crawled)
+    # Outputs list of article objects
 
-    print(card)
-    break
+    print(crawled.title)
+    # Outputs just titles of crawled articles
