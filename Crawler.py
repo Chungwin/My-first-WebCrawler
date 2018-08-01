5b24bf62038abb08d8a7c3ab2d8a094464486a64
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-08-01 21:25:15 +0200

    Apply a generator to have more control over loops and scrape more efficient

--- Crawler.py
+++ Crawler.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import
 from urllib.parse import urljoin
-# Import Timer
 import time
 
 
@@ -17,30 +16,21 @@ class ArticleFetcher():
     def fetch(self):
         url = "http://python.beispiel.programmierenlernen.io/index.php"
 
-            # Looping through multiple pages
-            # To stop the loop, set url = ""
         while url != "":
             print(url)
-            # Set a timer for the loop!! 2 sendonds ...
             time.sleep(2)
             r = requests.get(url)
             doc = BeautifulSoup(r.text, "html.parser")
 
-            # !!! PROBLEM: Every loop overrites the list from previous loop ...
-            articles = []
             for card in doc.select(".card"):
                 emoji = card.select_one(".emoji").text
                 content = card.select_one(".card-text").text
                 title = card.select(".card-title span")[1].text
                 image = urljoin (url, card.select_one("img").attrs["src"])
 
-                crawled = CrawledArticle(title, emoji, content, image)
-                articles.append(crawled)
+                yield CrawledArticle(title, emoji, content, image)
 
-            # When done with scraping articles from current site, select next page button
-            # If there is no next page button, stop the loop
             next_button = doc.select(".navigation .btn")
-
             if next_button:
                 next_href = next_button.attrs["href"]
                 next_href = urljoin(url, next_href)
@@ -48,10 +38,13 @@ class ArticleFetcher():
             else:
                 url = ""
 
-        return articles
-
 
 fetcher = ArticleFetcher()
-# Start fetching everything into the list
-articels = fetcher.fetch()
-# Outputs a list of objects
+
+# Now we can say e.g: Stop scraping after 8 articles ... 
+counter = 0
+for article in fetcher.fetch():
+    if counter == 8:
+        break
+    counter = counter + 1
+    print("Title: " + article.title)
