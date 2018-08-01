ad9ec524a3fbb3318d3c17564dbf2dbf16637d56
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-08-01 18:40:35 +0200

    Looping through multiple pages and set timer for the crawler

--- Crawler.py
+++ Crawler.py
@@ -1,7 +1,8 @@
-# Download html code from URL
 import requests
 from bs4 import
 from urllib.parse import urljoin
+# Import Timer
+import time
 
 
 class CrawledArticle():
@@ -12,22 +13,40 @@ class CrawledArticle():
         self.image = image
 
 
-# Whole fetcher is now in class ArticleFetcher()
 class ArticleFetcher():
     def fetch(self):
         url = "http://python.beispiel.programmierenlernen.io/index.php"
-        r = requests.get(url)
-        doc = BeautifulSoup(r.text, "html.parser")
 
-        articles = []
-        for card in doc.select(".card"):
-            emoji = card.select_one(".emoji").text
-            content = card.select_one(".card-text").text
-            title = card.select(".card-title span")[1].text
-            image = urljoin (url, card.select_one("img").attrs["src"])
+            # Looping through multiple pages
+            # To stop the loop, set url = ""
+        while url != "":
+            print(url)
+            # Set a timer for the loop!! 2 sendonds ...
+            time.sleep(2)
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text, "html.parser")
 
-            crawled = CrawledArticle(title, emoji, content, image)
-            articles.append(crawled)
+            # !!! PROBLEM: Every loop overrites the list from previous loop ...
+            articles = []
+            for card in doc.select(".card"):
+                emoji = card.select_one(".emoji").text
+                content = card.select_one(".card-text").text
+                title = card.select(".card-title span")[1].text
+                image = urljoin (url, card.select_one("img").attrs["src"])
+
+                crawled = CrawledArticle(title, emoji, content, image)
+                articles.append(crawled)
+
+            # When done with scraping articles from current site, select next page button
+            # If there is no next page button, stop the loop
+            next_button = doc.select(".navigation .btn")
+
+            if next_button:
+                next_href = next_button.attrs["href"]
+                next_href = urljoin(url, next_href)
+                url = next_href
+            else:
+                url = ""
 
         return articles
 
@@ -36,9 +55,3 @@ fetcher = ArticleFetcher()
 # Start fetching everything into the list
 articels = fetcher.fetch()
 # Outputs a list of objects
-print(articles)
-
-
-# Outfuts full URL of image because of urljoin
-for article in articles:
-    print(article.image)
